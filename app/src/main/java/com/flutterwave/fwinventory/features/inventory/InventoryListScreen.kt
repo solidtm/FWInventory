06c5778639625c8432e83f8d9e1848339400e432
lord-solid <mrichard@youverify.co>
2024-06-30 23:36:36 +0100

    Changed delete button position

--- app/src/main/java/com/flutterwave/fwinventory/features/inventory/InventoryListScreen.kt
+++ app/src/main/java/com/flutterwave/fwinventory/features/inventory/InventoryListScreen.kt
@@ -4,12 +4,14 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -91,17 +93,23 @@ fun InventoryListScreen(
 
 @Composable
 fun InventoryItemRow(item: InventoryItem, onClick: () -> Unit) {
-    Row(
+    Card(
         modifier = Modifier
             .fillMaxWidth()
             .clickable(onClick = onClick)
             .padding(8.dp)
+            .clip(RoundedCornerShape(5.dp)),
     ) {
-        Column(modifier = Modifier.weight(1f)) {
-            Text(text = item.name, style = MaterialTheme.typography.titleLarge)
-            Text(text = "Price: ${item.price}")
-            Text(text = "Total stock: ${item.totalStock}")
-            Text(text = "Description: ${item.description}")
+        Row(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = item.name, style = MaterialTheme.typography.titleLarge)
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(text = "Price: ${item.price}")
+                Text(text = "Total stock: ${item.totalStock}")
+                Text(text = "Description: ${item.description}")
+            }
         }
     }
 }
