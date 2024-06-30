d7bcebc2e4ecd8cf6872283a3d382eb21d4ce2d0
lord-solid <mrichard@youverify.co>
2024-06-30 14:01:21 +0100

    Added compose navigation

--- app/src/main/java/com/flutterwave/fwinventory/MainActivity.kt
+++ app/src/main/java/com/flutterwave/fwinventory/MainActivity.kt
@@ -3,45 +3,41 @@ package com.flutterwave.fwinventory
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.flutterwave.fwinventory.ui.theme.FWInventoryTheme
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.flutterwave.fwinventory.features.login.AuthViewModel
+import com.flutterwave.fwinventory.features.navigation.NavGraph
+import com.flutterwave.fwinventory.features.theme.FWInventoryTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            FWInventoryTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            val authViewModel: AuthViewModel = hiltViewModel()
+            val startDestination = if (authViewModel.isLoggedIn()) "inventory_list" else "login"
+            NavGraph(startDestination = startDestination)
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FWInventoryTheme {
-        Greeting("Android")
-    }
-}
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    FWInventoryTheme {
+//        Greeting("Android")
+//    }
+//}
